b6d2a1d14adf8a2e48cd2320368fb16e09c2aeb1
Bret Naylor <naylor.b@gmail.com>
2015-01-10 15:37:41 -0500

    simplify setup.py file

--- setup.py
+++ setup.py
@@ -1,11 +1,11 @@
 
-from setuptools import setup, find_packages
+from setuptools import setup
 
 setup(name='testflo',
       version='0.1',
       license = 'Apache 2.0',
       zip_safe=False,
-      packages=find_packages('src'),
+      packages=['testflo'],
       package_dir={'': 'src'},
       entry_points = {
           "console_scripts": [
