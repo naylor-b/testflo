508849e8957eaf739fbc888eade4e5dd0bddec79
Stephen Ryan <stephen.w.ryan@gmail.com>
2015-10-20 10:36:03 -0400

    add dummy test

--- testflo/test/test_testflo.py
+++ testflo/test/test_testflo.py
@@ -1,3 +1,12 @@
 import unittest
 
 # TODO: add some self-contained tests
+
+
+class TestFloTestCase(unittest.TestCase):
+
+    def test_upper(self):
+        self.assertEqual('foo'.upper(), 'FOO')
+
+if __name__ == '__main__':
+    unittest.main()
