735eff9bdfc5c02709ba23be941e45b6aedae145
kmarsteller <keith.g.marsteller@nasa.gov>
2018-07-17 10:22:33 -0400

    getting ready for 1.3.0 release

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@
 from distutils.core import setup
 
 setup(name='testflo',
-      version='1.2',
+      version='1.3',
       description="A simple flow based testing framework",
       license='Apache 2.0',
       install_requires=[
